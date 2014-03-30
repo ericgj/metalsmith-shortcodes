8773cb3379fa8d087e2fb22ad51a1ad57421a809
Eric Gjertsen <ericgj72@gmail.com>
2014-03-29 23:32:12 -0400

    add [x]content[/x] style shortcodes
    note content is injected as 'innerText'

--- test/index.js
+++ test/index.js
@@ -5,14 +5,23 @@ var Metalsmith = require('metalsmith');
 var subject = require('..');
 
 describe('metalsmith-shortcodes', function(){
-  it('should render a single shortcode', function(done){
-    Metalsmith('test/fixtures/basic')
-      .use(subject())
+
+  function testFixture(fix,opts,done){
+    Metalsmith('test/fixtures/' + fix)
+      .use(subject(opts))
       .build(function(err){
         if (err) return done(err);
-        equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
+        equal('test/fixtures/' + fix + '/expected', 'test/fixtures/' + fix + '/build');
         done();
       });
+  }
+
+  it('should render a single shortcode', function(done){
+    testFixture('basic',{},done);
+  });
+
+  it('should render shortcode with content', function(done){
+    testFixture('content',{},done);
   });
 
 })
